613096b952e4d9f8d11cc07d177d0332fcfcf936
cr3scents <cre5centsbusiness@gmail.com>
2025-02-13 11:26:23 -0600

    Setup
    - Configured Rider
    - Created SceneManager.cs

---
+++ Assets/Scripts/SceneManager.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class SceneManager : MonoBehaviour
+{
+    // START //
+    void Start()
+    {
+        
+    }
+
+    // UPDATE //
+    void Update()
+    {
+        
+    }
+}
